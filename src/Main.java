dd1a3fa7cb450d214290620bb2404c1c2b6e62b0
Chojnacki <twojemail@example.com>
2023-12-04 20:41:01 +0100

    dodanie obsługi dodawania

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
+        calculator.add(2, 2);
+        int result = calculator.add(2,2);
+        System.out.println(result);
     }
 }
